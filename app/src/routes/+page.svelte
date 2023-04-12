cf3441ff7cfc3c40a7f05250a1f183bfe354a67d
SilasCh <88002368+Kaszarr@users.noreply.github.com>
2023-04-12 14:58:40 +0200

    Did stuff

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,4 +1,59 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { flip } from 'svelte/animate';
+    let input = '';
+    let lix = 0;
+    let punctAmount;
+    let wordList;
+    let wordAmount;
+    let longWordAmount;
 
-<style lang="scss">@import "../styles/app.scss";</style>
+    function calculateLix(tekst)
+    {
+        if(tekst.length >= 1)
+           punctAmount = 0;
+           for(var i = 0; i < tekst.length; i++)
+           {
+               if(tekst[i] == '.')
+               {
+                   punctAmount += 1;
+               }
+           }
+           if (punctAmount < 1) 
+           {
+                punctAmount = 1
+           }
+           wordList = tekst.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '').split(' ');
+           wordAmount = wordList.length;
+           longWordAmount = 0;
+           for(var i = 0; i < wordList.length; i++)
+           {
+               if(wordList[i].length >= 7)
+               {
+                   longWordAmount += 1;
+               }
+           }
+           lix = Math.round((wordAmount / punctAmount) + (longWordAmount * 100 / wordAmount));
+    };
+
+</script>
+
+<head>
+    <style lang="scss">@import "../styles/app.scss";</style>
+    <title>LixRegner</title>
+</head>
+
+<body>
+    <h1>LixRegner</h1>
+    
+    <input placeholder="Tekst her!" on:input={()=>calculateLix(input)} bind:value={input}>
+    
+    <div>
+        <p1>Tekstens lixtal er {lix}</p1>
+        <p2>=55 Meget svær, faglitteratur på akademisk niveau, lovtekster.
+            45-54 Svær, f.eks. saglige bøger, populærvidenskabelige værker, akademiske udgivelser.
+            35-44 Middel, f.eks. dagblade og tidsskrifter.
+            25-34 Let for øvede læsere, f.eks. ugebladslitteratur og skønlitteratur for voksne.
+            =24 Let tekst for alle læsere, f.eks. børnelitteratur.</p2>
+    </div>
+
+</body>
